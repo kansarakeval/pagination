acde25ed99dd3a3ee7c8b5097b4c526efef73dde
os <kkeval304@gmail.com>
2024-06-17 18:11:54 +0530

    new

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pagination/screen/photo/view/photo_screen.dart';
+import 'package:pagination/screen/splash/view/splash_screen.dart';
 import 'package:pagination/screen/wallpaper/view/wallpaper_screen.dart';
-import 'package:pagination/splash/view/splash_screen.dart';
 
 Map<String,WidgetBuilder> app_routes={
   '/':(context) => const SplashScreen(),
